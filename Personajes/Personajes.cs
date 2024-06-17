e0632bb79c38d88e8c7a26f2490f66bf51f1d5ee
Hernan <hershins7@gmail.com>
2024-06-17 06:43:28 -0300

    editando clases en base a proyecto

--- Personajes/Personajes.cs
+++ Personajes/Personajes.cs
@@ -1,20 +1,43 @@
 namespace EspacioPersonajes
 {
-    public class Perosonaje
+    public class Personaje
     {
+        private Caracteristicas CaracteristicasPersonaje;  
         private Datos datosPeronaje;
-        private Caracteristicas CaracteristicasPersonaje;   
-
-        public Datos DatosPeronaje { get => DatoPeronaje; set => DatoPeronaje = value; }
+ 
         public Caracteristicas CaracteristicasPersonaje1 { get => CaracteristicasPersonaje; set => CaracteristicasPersonaje = value; }
+        public Datos DatosPeronaje { get => DatoPeronaje; set => DatoPeronaje = value; }
 
     }
 
-    public class Datos{
+    public class Caracteristicas
+    {
+        int velocidad; //1 a 10
+        int destreza; //1 a 5
+        int fuerza; //1 a 10
+        int nivel; //1 a 10
+        int armadura; //1 a 10
+        int salud; //100
 
+        public global::System.Int32 Velocidad { get => velocidad; set => velocidad = value; }
+        public global::System.Int32 Destreza { get => destreza; set => destreza = value; }
+        public global::System.Int32 Fuerza { get => fuerza; set => fuerza = value; }
+        public global::System.Int32 Nivel { get => nivel; set => nivel = value; }
+        public global::System.Int32 Armadura { get => armadura; set => armadura = value; }
+        public global::System.Int32 Salud { get => salud; set => salud = value; }
     }
 
-    public class Caracteristicas{
-        
+    public class Datos
+    {
+        tipo Tipo;
+        string Nombre;
+        string Apodo;
+        DateTime FechadeNacimiento;
+        int Edad; 
+    }
+
+    public enum tipo
+    {
+
     }
 }
