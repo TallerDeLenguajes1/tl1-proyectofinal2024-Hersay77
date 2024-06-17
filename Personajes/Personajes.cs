f29ae49efce818189c53dc099e19b6d6a2b8dbac
Hernan <hershins7@gmail.com>
2024-06-17 06:31:15 -0300

    editando-clases-personajes

---
+++ Personajes/Personajes.cs
@@ -0,0 +1,20 @@
+namespace EspacioPersonajes
+{
+    public class Perosonaje
+    {
+        private Datos datosPeronaje;
+        private Caracteristicas CaracteristicasPersonaje;   
+
+        public Datos DatosPeronaje { get => DatoPeronaje; set => DatoPeronaje = value; }
+        public Caracteristicas CaracteristicasPersonaje1 { get => CaracteristicasPersonaje; set => CaracteristicasPersonaje = value; }
+
+    }
+
+    public class Datos{
+
+    }
+
+    public class Caracteristicas{
+        
+    }
+}
